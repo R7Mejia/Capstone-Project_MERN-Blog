c025bf84b339f7577a3e254203b229cd5a764df6
Roberto Mejia <robertomejiar7@gmail.com>
2024-01-23 14:58:51 -0500

    ...

--- api/index.js
+++ api/index.js
@@ -29,8 +29,13 @@ const port = 2024;
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
 
-app.use(cors({ credentials: true, origin: "https://capstone-project-mern-blog.vercel.app/" }));
-// app.use(cors({  origin: "*" }));
+//  app.use(
+//    cors({
+//      credentials: true,
+//      origin: "https://capstone-project-mern-blog.vercel.app",
+//    })
+//  );
+app.use(cors({  origin: "*" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
